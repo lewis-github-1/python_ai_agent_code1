0868f35cf5c953d913ae5917db8f4f9a092927d3
lewis-github-1 <ltowne2@gmail.com>
2025-08-26 23:38:43 -0500

    added get_file_content.py, get_files_info.py, tests.py, and lorem.txt. need to fix bugs

---
+++ functions/get_file_content.py
@@ -0,0 +1,28 @@
+import os
+from functions.config import MAX_FILE_CHARS
+
+def get_file_content(working_directory, file_path):
+    try:
+        path = os.path.join(working_directory, file_path)
+        abs_path = os.path.abspath(path)
+        abs_work = os.path.abspath(working_directory)
+
+        if not abs_path.startswith(abs_work):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not abs_path.isdir(abs_path):
+            return f'Error: File not found or is not a regular file: {file_path}'
+        
+        with open(abs_path, 'r') as f:
+            file_content_string = f.read(MAX_FILE_CHARS)
+            
+        if len(file_content_string) > MAX_FILE_CHARS:
+            file_content_string = file_content_string[:MAX_FILE_CHARS]
+            file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+
+
+    except Exception as e:
+        return f'Error: {str(e)}'
+
+
+
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,4 +4,23 @@ def get_files_info(working_directory, directory="."):
     path = os.path.join(working_directory, directory)
     abs_path = os.path.abspath(path)
     abs_work = os.path.abspath(working_directory)
-    
+
+    if not abs_path.startswith(abs_work):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    if not os.path.isdir(abs_path):
+        return f'Error: "{directory}" is not a directory'
+
+    lines = []
+    try:
+        for i in os.listdir(abs_path):
+            item_path = os.path.join(abs_path, i)
+            get_size = os.path.getsize(item_path)
+            is_dir = os.path.isdir(item_path)
+            lines.append(f'- {i}: file_size={get_size} bytes, is_dir={is_dir}')
+
+        return '\n'.join(lines)
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
