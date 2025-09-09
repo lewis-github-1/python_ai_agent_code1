aa81db30c69b1f4756c848f671dd3ccaeb673f66
lewis-github-1 <ltowne2@gmail.com>
2025-09-08 21:27:31 -0500

    added the system_prompt variable in main to change how the llm responds

--- main.py
+++ main.py
@@ -30,10 +30,14 @@ def main():
     messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),   
     ]
+
+    system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
         
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents=messages)
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        )
     
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
