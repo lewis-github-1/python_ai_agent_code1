deaf8e066c2b6a26cd18ba151bc769bb7accaec1
lewis-github-1 <ltowne2@gmail.com>
2025-09-21 21:55:08 -0500

    added call_function function and refactored the code so the agent can call one of the functions

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from functions.get_files_info import schema_get_files_info
 from functions.get_file_content import schema_get_file_content
 from functions.run_python_file import schema_run_python_file
 from functions.write_file import schema_write_file
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
 
 
 load_dotenv()
@@ -74,7 +78,9 @@ def main():
 
     if response.function_calls:
         for fc in response.function_calls:
-            print(f"Calling function: {fc.name}({fc.args})")
+            result_content = call_function(fc, verbose=verbose)
+            if verbose:
+                print(f"-> {result_content.parts[0].function_response.response}")
     else:
         print(response.text)
 
@@ -83,7 +89,43 @@ def main():
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
               
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = dict(function_call_part.args or {})
 
+    if verbose:
+        print(f"Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}")
+    
+    function_args["working_directory"] = "./calculator"
+    functions = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+        }
+
+    func = functions.get(function_name)
+    if func is None:
+        return types.Content(
+            role="tool",
+            parts=[types.Part.from_function_response(
+                name=function_name,
+                response={"error": f"Unknown function: {function_name}"},
+            )],
+        )
+    function_result = func(**function_args)
+
+    return types.Content(
+        role="tool",
+        parts=[types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+        )],
+    )
+
+    
 
 if __name__ == "__main__":
     main()
